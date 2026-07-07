b29a625fc106b27ae5ef5bbd6c1de07b97bdc5f1
Charan Daraboina <daraboinacharan@gmail.com>
2026-07-07 12:50:53 +0000

    Clean

--- backend/test-connect.mjs
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const p = new PrismaClient();
-try {
-  await p.$connect();
-  console.log("DB_CONNECT_OK");
-} catch (e) {
-  console.log("DB_CONNECT_FAIL: " + (e?.message?.split("\n")[0] || e));
-}
-await p.$disconnect();
